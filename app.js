719234753ca96213c42ab5bfdfff3c9c82077128
Aung Yang <aungkyang622@gmail.com>
2019-08-04 21:33:01 -0700

    set up node.js for weather app

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+
+app.use(express.static("public"));
+
+
+app.get("/", function (req, res) {
+    res.render("search.ejs");
+});
+
+
+app.get("/results", function (req, res){
+    let lat = req.query.search;
+    let long = req.query.search2;
+    
+    let api_key = "213937730f592bdd0b26e4a5a2972666";
+    //us is the best one.
+    let url = `https://api.darksky.net/forecast/${api_key}/${lat},${long}?units=us`
+
+
+
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            let data = JSON.parse(body)
+
+            res.render("results.ejs", {data: data});
+    
+        } else {
+            res.send("Your input is invalid! ");
+        }
+
+    })
+})
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server started!")
+})
+
+
