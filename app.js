495aadcf73eb82fcc860f8f7d9d77e5f609e6c78
Aung Yang <aungkyang622@gmail.com>
2019-08-08 13:29:21 -0700

    integrated google maps api

--- app.js
+++ app.js
@@ -5,30 +5,48 @@ var request = require("request");
 app.use(express.static("public"));
 
 
+
+
 app.get("/", function (req, res) {
+    
     res.render("search.ejs");
 });
 
 
+
+
 app.get("/results", function (req, res){
-    let lat = req.query.search;
-    let long = req.query.search2;
+    let userInput = req.query.search;
     
-    let api_key = "213937730f592bdd0b26e4a5a2972666";
-    //us is the best one.
-    let url = `https://api.darksky.net/forecast/${api_key}/${lat},${long}?units=us`
+    //API KEYS
+    let geocode_api_key = "AIzaSyAZpBUG4CpsIAk6dT73MbOUyC6f99hUXJs"
+    let dark_sky_api_key = "213937730f592bdd0b26e4a5a2972666";
+
+    let geocode_url = `https://maps.googleapis.com/maps/api/geocode/json?address=${userInput}&key=${geocode_api_key}`
 
 
 
-    request(url, function (error, response, body) {
+    request(geocode_url, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             let data = JSON.parse(body)
 
-            res.render("results.ejs", {data: data});
-    
-        } else {
-            res.send("Your input is invalid! ");
-        }
+            let lat = (data["results"].find(o => o.location_type = "APPROXIMATE").geometry.location.lat);
+            
+            let lng = (data["results"].find(o => o.location_type = "APPROXIMATE").geometry.location.lng); 
+            
+
+            let dark_sky_url = `https://api.darksky.net/forecast/${dark_sky_api_key}/${lat},${lng}?units=us`
+
+            request(dark_sky_url, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    let data = JSON.parse(body);
+
+                    res.render("results.ejs", { data: data });
+                } else {
+                    res.send(`Sorry something went wrong. ERROR: ${error}`)
+                }
+            })
+        } 
 
     })
 })
